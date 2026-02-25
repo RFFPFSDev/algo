b207a5eb6f59ae56646c6068563ce24a6d135ced
Roberto Freire <roberto.freire@valtech.com>
2026-02-25 10:44:56 -0300

    RemoveAdjacentDuplicates

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -9,21 +9,22 @@ class Program
 
     static void RunTests()
     {
-        Test("abcabcbb", "abc");
-        Test("bbbbb", "b");
-        Test("aa", "a");
-        Test("ab", "ab");
-        Test("c", "c");
-        Test("pwwkew", "wke");
-        Test("wpwkew", "pwke");
+        Test("abbaca", "ca");
+        Test("azxxzy", "ay");
+        Test("aabbcc", "");
+        Test("abc", "abc");
+        Test("aaaa", "");
+        Test("abba", "");
+        Test("aabccbadd", "d");
         Test("", "");
-        Test("abcdef", "abcdef");
-        Test("abccefg", "cefg");
+        Test("a", "a");
+        Test("aaabccddd", "abd");
     }
 
     static void Test(string input, string expected)
     {
-        var result = LongestDistinctSubstring(input);
+        var result = RemoveAdjacentDuplicates(input);
+
         if (result == expected)
         {
             Console.WriteLine($"✅: \"{input}\" -> \"{result}\"");
@@ -34,38 +35,7 @@ class Program
         }
     }
 
-    static string LongestDistinctSubstring(string s)
+    static string RemoveAdjacentDuplicates(string s)
     {
-        int len = s.Length;
-        if (len <= 1)
-        {
-            return s;
-        }
-
-        int l = 0;
-        int startIndex = l;
-        int maxLength = 1;
-        var unique = new HashSet<char>();
-        unique.Add(s[l]);
-        int r = 1;
-
-        while (r < len)
-        {
-            if (unique.Add(s[r]))
-            {
-                if (maxLength < r - l + 1)
-                {
-                    maxLength = r - l + 1;
-                    startIndex = l;
-                }
-                r++;
-                continue;
-            }
-
-            unique.Remove(s[l]);
-            l++;
-        }
-
-        return s.Substring(startIndex, maxLength);
     }
 }
--- README.md
+++ README.md
@@ -631,3 +631,76 @@ class Program
     }
 }
 ```
+
+### Stack
+
+#### Repeatedly remove adjacent duplicate characters until no duplicates remain
+
+```cs
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RunTests();
+    }
+
+    static void RunTests()
+    {
+        Test("abbaca", "ca");
+        Test("azxxzy", "ay");
+        Test("aabbcc", "");
+        Test("abc", "abc");
+        Test("aaaa", "");
+        Test("abba", "");
+        Test("aabccbadd", "d");
+        Test("", "");
+        Test("a", "a");
+        Test("aaabccddd", "abd");
+    }
+
+    static void Test(string input, string expected)
+    {
+        var result = RemoveAdjacentDuplicates(input);
+
+        if (result == expected)
+        {
+            Console.WriteLine($"✅: \"{input}\" -> \"{result}\"");
+        }
+        else
+        {
+            Console.WriteLine($"❌: \"{input}\" | Expected: \"{expected}\", Got: \"{result}\"");
+        }
+    }
+
+    static string RemoveAdjacentDuplicates(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return s;
+
+        var stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (stack.Count > 0 && stack.Peek() == c)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+
+        var result = new StringBuilder();
+
+        while (stack.Count > 0)
+        {
+            result.Insert(0, stack.Pop());
+        }
+
+        return result.ToString();
+    }
+}
+```
