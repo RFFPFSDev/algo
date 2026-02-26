b1a88e93f0e91d17ac31f36558f6561ff28a353d
Roberto Freire <roberto.freire@valtech.com>
2026-02-26 15:59:16 -0300

    Update Math

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,24 +6,20 @@
         Console.WriteLine($"8 is even? {8 % 2 == 0}");
         Console.WriteLine($"9 is odd? {9 % 2 != 0}");
         double division = 5.0 / 2.0;
-        int truncated = (int)division;
         Console.WriteLine($"5.0 / 2.0 = {division}");
-        Console.WriteLine($"After casting to int: {truncated}");
+        Console.WriteLine($"(int)2.5: {(int)division}");
         double division2 = 5 / 2;
         Console.WriteLine($"5 / 2 = {division2}");
         double division3 = ((double)5) / 2;
         Console.WriteLine($"((double)5) / 2 = {division3}");
-        int x = 10;
-        int y = 25;
-        Console.WriteLine($"X,Y={x},{y}");
-        Console.WriteLine($"Min: {Math.Min(x, y)}");
-        Console.WriteLine($"Max: {Math.Max(x, y)}");
-        Console.WriteLine($"Floor(4.1) = {Math.Floor(4.1)}");
-        Console.WriteLine($"Floor(4.9) = {Math.Floor(4.9)}");
-        Console.WriteLine($"Ceiling(4.1) = {Math.Ceiling(4.1)}");
-        Console.WriteLine($"Ceiling(4.9) = {Math.Ceiling(4.9)}");
-        Console.WriteLine($"Round(4.3) = {Math.Round(4.3)}");
-        Console.WriteLine($"Round(4.5) = {Math.Round(4.5)}");
-        Console.WriteLine($"Round(4.6) = {Math.Round(4.6)}");
+        Console.WriteLine($"Math.Min(10, 25) = {Math.Min(10, 25)}");
+        Console.WriteLine($"Math.Max(10, 25) = {Math.Max(10, 25)}");
+        Console.WriteLine($"Math.Floor(4.1) = {Math.Floor(4.1)}");
+        Console.WriteLine($"Math.Floor(4.9) = {Math.Floor(4.9)}");
+        Console.WriteLine($"Math.Ceiling(4.1) = {Math.Ceiling(4.1)}");
+        Console.WriteLine($"Math.Ceiling(4.9) = {Math.Ceiling(4.9)}");
+        Console.WriteLine($"Math.Round(4.3) = {Math.Round(4.3)}");
+        Console.WriteLine($"Math.Round(4.5) = {Math.Round(4.5)}");
+        Console.WriteLine($"Math.Round(4.6) = {Math.Round(4.6)}");
     }
 }
